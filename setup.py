e11f36b240dac722a445b3d8e255ab32094a332e
Bastien Gerard <bastien.gerard@cluepoints.com>
2021-08-21 21:48:08 +0200

    minor edit in setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,21 @@ INSTALL_REQUIRES = [
     "ruamel.yaml>=0.16.0",
 ]
 
+VERSION = "0.0.0"
+
 setup(
     name="gitlabci-jsonschema-lint",
+    version=VERSION,
+    maintainer="Bastien Gerard",
+    maintainer_email="bast.gerard@gmail.com",
+    url="https://github.com/bagerard/gitlabci-jsonschema-lint",
+    keywords="gitlab-ci linter ",
+    license="MIT License",
     packages=["gitlabci_lint"],
     install_requires=INSTALL_REQUIRES,
     entry_points={"console_scripts": ["gitlabci-jsonschema-lint = gitlabci_lint:main"]},
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Quality Assurance",
     ],
 )
